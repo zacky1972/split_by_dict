f36a90be479199914f189463d83c07e73e7d751c
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-20 19:46:50 +0900

    set up interface of split_by_dict

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SplitByDict
 
-A string splitter by a map as a dictionary.
+A string splitter by a list as a dictionary.
 
 @shoz-f provided this problem: https://twitter.com/ShozF/status/1560820784188116993
 
--- lib/split_by_dict.ex
+++ lib/split_by_dict.ex
@@ -1,7 +1,15 @@
 defmodule SplitByDict do
   @moduledoc """
-  A string splitter by a map as a dictionary.
+  A string splitter by a list as a dictionary.
 
   @shoz-f provided this problem: https://twitter.com/ShozF/status/1560820784188116993
   """
+
+  @doc """
+  Splits the given `str` by `dict`.
+  """
+  @spec split_by_dict(String.t(), list(String.t())) :: String.t()
+  def split_by_dict(_str, _dict) do
+    ""
+  end
 end
