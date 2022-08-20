a0bc19dd3ce7bcb901b217fdc1d65db02f1ea630
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-20 19:17:38 +0900

    initial commit

---
+++ lib/split_by_dict.ex
@@ -0,0 +1,18 @@
+defmodule SplitByDict do
+  @moduledoc """
+  Documentation for `SplitByDict`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SplitByDict.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
